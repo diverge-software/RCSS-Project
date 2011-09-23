ea065a235850e707792e7d7ec0ced0529ad81b2f
Jared Mar <jaredfrommars@gmail.com>
2011-09-23 17:13:24 -0500

    Added revised sense body parser. Returns struct instead of a hash table.

---
+++ sense_body_parser.h
@@ -0,0 +1,13 @@
+// sense_body_parser.h
+
+#ifndef SENSE_BODY_PARSER_H
+#define SENSE_BODY_PARSER_H
+
+#include <string>
+#include "sense_body_parser.cpp"
+using namespace std;
+
+senseBodyData parse_sense_body(string inData);
+
+#endif
+
---
+++ test_sense_body_parser.cpp
@@ -0,0 +1,76 @@
+// testsbp.cpp
+
+#include <string>
+#include <iostream>
+#include <unordered_map>
+#include "sense_body_parser.h"
+
+void printParsedData(senseBodyData hashData);
+
+int main()
+{
+	// example server data
+	string inData = "(sense_body 142 (view_mode high normal) (stamina 8000 1 127530) (speed 0.01 0) (head_angle 0) (kick 1) (dash 121) (turn 34) (say 0) (turn_neck 0) (catch 0) (move 1) (change_view 0) (arm (movable 51) (expires 52) (target 53 54) (count 55)) (focus (target none) (count 0)) (tackle (expires 0) (count 0)) (collision none) (foul  (charged 0) (card none)))";
+
+	cout << inData << endl << endl;
+
+	// Create senseBodyData instance to hold the parsed data
+	senseBodyData sbd;
+	
+	// parse the server data and return the struct
+	sbd = parse_sense_body(inData);
+
+	// print out the struct table
+	printParsedData(sbd);
+}
+
+
+void printParsedData(senseBodyData sbd)
+{	
+	cout << "timestamp" << ": " << sbd.timestamp << endl;
+	
+	cout << "view_mode" << ": " << sbd.view_mode.viewQuality << " "
+	                            << sbd.view_mode.viewWidth << endl;
+	
+	cout << "stamina" << ": " << sbd.stamina[0] << " "
+	                          << sbd.stamina[1] << " "
+	                          << sbd.stamina[2] << endl;
+	
+	cout << "speed" << ": " << sbd.speed[0] << " "
+	                        << sbd.speed[1] << endl;
+	
+	cout << "head_angle" << ": " << sbd.actionCount << endl;
+	
+	cout << "kick" << ": " << sbd.actionCount << endl;
+	
+	cout << "dash" << ": " << sbd.actionCount << endl;
+	
+	cout << "turn" << ": " << sbd.actionCount << endl;
+	
+	cout << "say" << ": " << sbd.actionCount << endl;
+	
+	cout << "turn_neck" << ": " << sbd.actionCount << endl;
+	
+	cout << "catch" << ": " << sbd.actionCount << endl;
+	
+	cout << "move" << ": " << sbd.actionCount << endl;
+	
+	cout << "change_view" << ": " << sbd.actionCount << endl;
+	
+	cout << "arm" << ": " << sbd.arm.movable << " "
+	                      << sbd.arm.expires << " "
+	                      << sbd.arm.target[0] << " "
+	                      << sbd.arm.target[1] << " "
+	                      << sbd.arm.count << endl;
+	
+	cout << "focus" << ": " << sbd.focus.target << " "
+	                        << sbd.focus.count << endl;
+	
+	cout << "tackle" << ": " << sbd.tackle.expires << " "
+	                         << sbd.tackle.count << endl;
+	
+	cout << "collision" << ": " << sbd.collision << endl;
+	
+	cout << "foul" << ": " << sbd.foul.charged << " "
+	                       << sbd.foul.card << endl;
+}
