590cd7dacb35337f03f4be643d841ddb631491e1
Keeler Russell <keeler-russell@ku.edu>
2011-10-14 00:52:17 -0500

    Added javadoc comments to the Player.hpp file.

--- Player.hpp
+++ Player.hpp
@@ -1,3 +1,8 @@
+/** @file Player.hpp
+* Represents a 2-dimensional vector.
+* @author Keeler Russell, Jared Mar, Corbin Charpentier
+* @date Oct 13, 2011
+*/
 #ifndef PLAYER_HPP
 #define PLAYER_HPP
 
@@ -15,20 +20,67 @@ using namespace Parser;
 #define NUM_PLAYER_TYPES 17
 #define MAX_QUEUE_SIZE 10
 
+/** @class Player
+* A class which represents a player: memory, sensory processing, and thinking.
+*/
 class Player
 {
 	public:
+		/* Default constructor. Initializes invalid values for aural and sense body queues.
+		* Also initializes stationary flag positions.
+		* @pre None.
+		* @post This object is ready to receive server messages and parse/store their data.
+		*/
 		Player();
-		// parseBuffer should eventually be a private member
+		/* Parses a buffer of information from the soccer server.
+		* @param buffer Any S-expression string sent from the soccer server.
+		* @pre None.
+		* @post The relevant data will be stored in the private members of this object based
+		* on the type of message parsed. If an error occured, this function will hit an assertion.
+		*/
 		bool parseBuffer( const string buffer );
 
-		// Printing functions for parsed data
+		/* Prints the most recently stored visual information hash to the specified output stream.
+		* @param os The output stream to write to.
+		* @pre None.
+		* @post The contents of the hash will be printed to the output stream.
+		*/
 		void printNewestVisualHash( ostream & os );
+		/* Prints the most recently stored list of visible players to the specified output stream.
+		* @param os The output stream to write to.
+		* @pre None.
+		* @post The contents of the visible player vector will be printed to the output stream.
+		*/
 		void printNewestVisiblePlayersList( ostream & os );
+		/* Prints the most recently stored aural information struct to the specified output stream.
+		* @param os The output stream to write to.
+		* @pre None.
+		* @post The contents of the aural struct will be printed to the output stream.
+		*/
 		void printNewestAuralStruct( ostream & os );
+		/* Prints the most recently stored sense body information struct to the specified output stream.
+		* @param os The output stream to write to.
+		* @pre None.
+		* @post The contents of the sense body struct will be printed to the output stream.
+		*/
 		void printNewestSenseBodyStruct( ostream & os );
+		/* Prints the contents of the server information hash to the specified output stream.
+		* @param os The output stream to write to.
+		* @pre None.
+		* @post The contents of the hash will be printed to the output stream.
+		*/
 		void printServerHash( ostream & os );
+		/* Prints the contents of the player types hash to the specified output stream.
+		* @param os The output stream to write to.
+		* @pre None.
+		* @post The contents of the hash will be printed to the output stream.
+		*/
 		void printPlayerTypesHash( ostream & os );
+		/* Prints the contents of the palyer parameter hash to the specified output stream.
+		* @param os The output stream to write to.
+		* @pre None.
+		* @post The contents of the hash will be printed to the output stream.
+		*/
 		void printPlayerParamHash( ostream & os );
 
 	private:
