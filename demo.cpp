eb6da185f80dbc9b50dcc7ceedb7e364c952a57a
Jared Mar <jaredfrommars@gmail.com>
2011-10-18 20:56:09 -0500

    Got demo.cpp to send messages to the server.

---
+++ demo.cpp
@@ -0,0 +1,81 @@
+#include "udp_client.hpp"
+#include "demo.hpp"
+//#include <string>
+
+string movePlayersOntoField(unsigned int playerNum)
+{
+
+    switch (playerNum)
+        {
+        case 0:
+            return "(move -10 10)";
+            break;
+        case 1:
+            return "(move -5 0)";
+            break;
+        case 2:
+            return "(move -10 0)";
+            break;
+        case 3:
+            return "(move -15 0)";
+            break;
+        case 4:
+            return "(move -20 0)";
+            break;
+        case 5:
+            return "(move -5 5)";
+            break;
+        case 6:
+            return "(move -5 -5)";
+            break;
+        case 7:
+            return "(move -5 10)";
+            break;
+        case 8:
+            return "(move -5 -10)";
+            break;
+        case 9:
+            return "(move -5 15)";
+            break;
+        case 10:
+            return "(move -5 -15)";
+            break;
+        }
+
+/*
+	if(playerNum == 4)
+	{
+		return "(move 10 10)";
+	}
+	else
+	{
+		return "don't do anything";
+	}
+*/
+}
+
+string makePlayersRunAroundOnField(unsigned int playerNum)
+{
+
+	return "(dash 30)";
+
+/*
+	if(playerNum == 4)
+	{        
+		return "(dash 20)";
+    }
+	else
+	{
+		return "don't do anything";
+	}
+*/
+}
+
+string makeThemMove(unsigned int playerNum)
+    {
+        string serverString;
+
+ 
+                return ( movePlayersOntoField(playerNum) );
+                return ( makePlayersRunAroundOnField(playerNum) );
+	}
---
+++ demo.hpp
@@ -0,0 +1,6 @@
+#include "udp_client.hpp"
+#include <string>
+
+string movePlayersOntoField(unsigned int playerNum);
+
+string makePlayersRunAroundOnField(unsigned int playerNum);
