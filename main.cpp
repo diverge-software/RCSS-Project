84f05a63b1f62635e46873308369fcae78c4895c
Joseph Wachtel <jmwachtel@gmail.com>
2011-11-08 23:43:04 -0600

    Modified move command and a function to tokenize a string.  Added functionality to choose how many players to enable and of which types.

--- main.cpp
+++ main.cpp
@@ -50,6 +50,12 @@ using namespace std;
                               PROCEDURES
 --------------------------------------------------------------------*/
 
+static vector< string > split_str   /* split string into tokens     */
+    ( 
+    string              in_str      /* input string                 */
+    );
+
+
 /** Main program processing
 * @param argc Number of input arguments
 * @param *argv[] An array of the inputs arguments
@@ -68,13 +74,18 @@ int main							/* main program processing		*/
 /*----------------------------------------------------------
 Local Variables
 ----------------------------------------------------------*/
+int                     defender;   /* defender count               */
 boolean                 error;      /* error occurred               */
+int                     forward;    /* forward count                */
+int                     goalie;     /* goalie count                 */
 int                     i;          /* index                        */
 string                  input;      /* input                        */
+int                     midfielder; /* midfielder count             */
 int                     player_cnt; /* player count                 */
 string                  pt_str;     /* player type string           */
 string                  server_ip;  /* server IP                    */
 unsigned int            server_port;/* server port                  */
+int                     trainer;    /* trainer count                */
 boolean                 trainer_enbld;
                                     /* trainer enabled              */
 ostringstream           tmp_str;    /* temporary string             */
@@ -83,10 +94,7 @@ UDP_client *            udp_client[ CLIENT_CNT ];
 UDP_client *            udp_client_ptr;
                                     /* UDP client pointer           */
 int                     uniform_num;/* uniform number               */
-
-string                  object_str;
-string                  x_str;
-string                  y_str; 
+vector< string >        vec_str;    /* vector string                */
 
 /*----------------------------------------------------------
 Get server information for user
@@ -110,11 +118,77 @@ cout << endl;
 if( (input.compare( "y" ) == 0) || (input.compare( "Y" ) == 0) )
     {
     trainer_enbld = TRUE;
-    player_cnt = CLIENT_CNT;
+
+    /*------------------------------------------------------
+    Specify goalie enabled
+    ------------------------------------------------------*/
+    cout << "Enable the goalie [y/n]: ";
+    cin >> input;
+
+    if( (input.compare( "y" ) == 0) || (input.compare( "Y" ) == 0) )
+        {
+        goalie = 1;
+        }
+    else
+        {
+        goalie = 0;
+        }
+
+    /*------------------------------------------------------
+    Specify number of forwards to enable
+    ------------------------------------------------------*/
+    cout << "Number of forwards to enable [0-3]: ";
+    cin >> input;
+
+    while( atoi( input.c_str() ) > 3 )
+        {
+        cout << "Invalid input" << endl;
+        cin >> input;
+        }
+
+    forward = atoi( input.c_str() );
+
+    /*------------------------------------------------------
+    Specify number of defenders to enable
+    ------------------------------------------------------*/
+    cout << "Number of defenders to enable [0-3]: ";
+    cin >> input;
+
+    while( atoi( input.c_str() ) > 3 )
+        {
+        cout << "Invalid input" << endl;
+        cin >> input;
+        }
+
+    defender = atoi( input.c_str() );
+
+    /*------------------------------------------------------
+    Specify number of midfielders to enable
+    ------------------------------------------------------*/
+    cout << "Number of midfielders to activate [0-4]: ";
+    cin >> input;
+
+    while( atoi( input.c_str() ) > 4 )
+        {
+        cout << "Invalid input" << endl;
+        cin >> input;
+        }
+
+    midfielder = atoi( input.c_str() );
+
+    cout << endl;
+    trainer = 1;
+    player_cnt = goalie + defender + forward + midfielder + trainer;
     }
 else
     {
     trainer_enbld = FALSE;
+
+    goalie     = 1;
+    defender   = 3;
+    midfielder = 4;
+    forward    = 3;
+
     player_cnt = CLIENT_CNT - 1;
     }
 
@@ -129,6 +203,7 @@ Assign player types for each soccer client
 for( i = 0; i < player_cnt; i++ )
     {   
     udp_client_ptr = new UDP_client;
+    pt_str.clear();
 
     /*------------------------------------------------------
     Enable debug logging for client 
@@ -140,7 +215,7 @@ for( i = 0; i < player_cnt; i++ )
     /*------------------------------------------------------
     Assign player goalie type 
     ------------------------------------------------------*/
-    if( i == 0 )
+    if( goalie-- > 0 )
         {
         uniform_num = udp_client_ptr->UDP_open_socket( server_ip, server_port, TEAM_NAME, PLAYER_TYPE_GOALIE );
         pt_str = "Goalie";
@@ -149,8 +224,7 @@ for( i = 0; i < player_cnt; i++ )
     /*------------------------------------------------------
     Assign player defender type 
     ------------------------------------------------------*/
-    else if( ( i > 0 )
-          && ( i < 4 ) )
+    else if( defender-- > 0 )
         {
         uniform_num = udp_client_ptr->UDP_open_socket( server_ip, server_port, TEAM_NAME, PLAYER_TYPE_DEFENDER );
         pt_str = "Defender";
@@ -159,8 +233,7 @@ for( i = 0; i < player_cnt; i++ )
     /*------------------------------------------------------
     Assign player forward type 
     ------------------------------------------------------*/
-    else if( ( i >= 4 )
-          && ( i < 8  ) )
+    else if( forward-- > 0 )
         {
         uniform_num = udp_client_ptr->UDP_open_socket( server_ip, server_port, TEAM_NAME, PLAYER_TYPE_FORWARD );
         pt_str = "Forward";
@@ -169,8 +242,7 @@ for( i = 0; i < player_cnt; i++ )
     /*------------------------------------------------------
     Assign player midfielder type 
     ------------------------------------------------------*/
-    else if( ( i >= 8 )
-          && ( i < 11 ) )
+    else if( midfielder-- > 0 )
         {
         uniform_num = udp_client_ptr->UDP_open_socket( server_ip, server_port, TEAM_NAME, PLAYER_TYPE_MIDFIELDER );
         pt_str = "Midfielder";
@@ -179,23 +251,26 @@ for( i = 0; i < player_cnt; i++ )
     /*------------------------------------------------------
     Assign trainer type
     ------------------------------------------------------*/
-    else if( trainer_enbld )
+    else if( trainer-- > 0 )
         {
         uniform_num = udp_client_ptr->UDP_open_socket( server_ip, server_port + 1, TEAM_NAME, PLAYER_TYPE_TRAINER );
         pt_str = "Trainer";
         }
 
-    if( uniform_num == -1 )
+    if( pt_str.compare( "" ) != 0 )
         {
-        cout << endl << "Error: Client unable to connect to the RoboCup Soccer Server" << endl;
-        error = TRUE;
+        if( uniform_num == -1 )
+            {
+            cout << endl << "Error: Client unable to connect to the RoboCup Soccer Server" << endl;
+            error = TRUE;
 
-        break;
-        }
-    else
-        {
-        udp_client[ uniform_num - 1 ] = udp_client_ptr;
-        cout << "Uniform Number: " << uniform_num << " Player Type: " << pt_str << endl;
+            break;
+            }
+        else
+            {
+            udp_client[ uniform_num - 1 ] = udp_client_ptr;
+            cout << "Uniform Number: " << uniform_num << " Player Type: " << pt_str << endl;
+            }
         }
     }
 
@@ -210,8 +285,7 @@ if( !error )
         {
         cout << "sp: Stop drills" << endl;
         cout << "st: Start drills" << endl;
-        cout << "mp: Move player [mp uniform x y]" << endl;
-        cout << "mb: Move ball [mb x y]" << endl;
+        cout << "mv: Move player/ball [mv [b/p] [uniform] x y]" << endl;
         }
 
     cout << "rw: Send raw command to server" << endl;
@@ -222,15 +296,28 @@ if( !error )
         {
         cout << "Command: ";
         getline( cin, input );
+        vec_str = split_str( input );
 
         /*--------------------------------------------------
         Quit command
         --------------------------------------------------*/
-        if( input.compare( "qt" ) == 0 )
+        if( vec_str[ 0 ].compare( "qt" ) == 0 )
             {
-            for( i = 0; i < player_cnt; i++ )
+            if( trainer_enbld )
                 {
-                udp_client[ i ]->UDP_close_socket();
+                for( i = 0; i < player_cnt - 1; i++ )
+                    {
+                    udp_client[ i ]->UDP_close_socket();
+                    }
+
+                udp_client[ 11 ]->UDP_close_socket();
+                }
+            else
+                {
+                for( i = 0; i < player_cnt; i++ )
+                    {
+                    udp_client[ i ]->UDP_close_socket();
+                    }
                 }
 
             return( 0 );
@@ -239,8 +326,8 @@ if( !error )
         /*--------------------------------------------------
         Stop drill command
         --------------------------------------------------*/
-        else if( ( input.compare( "sp" ) == 0 )
-              && ( trainer_enbld              ) )
+        else if( ( vec_str[ 0 ].compare( "sp" ) == 0 )
+              && ( trainer_enbld                     ) )
             {
             udp_client[ TRNR_UNUM - 1 ]->UDP_send( "(change_mode time_over)" );
             }
@@ -248,54 +335,56 @@ if( !error )
         /*--------------------------------------------------
         Start drill command
         --------------------------------------------------*/
-        else if( ( input.compare( "st" ) == 0 )
-              && ( trainer_enbld              ) )
+        else if( ( vec_str[ 0 ].compare( "st" ) == 0 )
+              && ( trainer_enbld                     ) )
             {
             udp_client[ TRNR_UNUM - 1 ]->UDP_send( "(change_mode play_on)" );
             }
 
         /*--------------------------------------------------
-        Move player command
+        Move command
         --------------------------------------------------*/
-        else if( ( input.find( "mp" ) == 0 )
-              && ( trainer_enbld           ) )
+        else if( ( vec_str[ 0 ].find( "mv" ) == 0 )
+              && ( trainer_enbld                  ) )
             {
-            input.erase( 0, 3 );
+            if( vec_str[ 1 ].compare( "b" ) == 0 )
+                {
+                if( vec_str.size() < 4 )
+                    {
+                    cout << "Invalid Format" << endl;
+                    }
+                else
+                    {
+                    tmp_str.str( "" );
+                    tmp_str << "(move (ball) " << vec_str[ 2 ] << " " << vec_str[ 3 ] << ")";
 
-            object_str = input.substr( 0, input.find_first_of( " " ) );
-            input.erase( 0, input.find_first_of( " " ) + 1 );
-            x_str = input.substr( 0, input.find_first_of( " " ) );
-            input.erase( 0, input.find_first_of( " " ) + 1 );
-            y_str = input.substr( 0, input.length() );
+                    udp_client[ TRNR_UNUM - 1 ]->UDP_send( tmp_str.str() );
+                    }
+                }
+            else if( vec_str[ 1 ].compare( "p" ) == 0 )
+                {
+                if( vec_str.size() < 5 )
+                    {
+                    cout << "Invalid Format" << endl;
+                    }
+                else
+                    {
+                    tmp_str.str( "" );
+                    tmp_str << "(move (player " << TEAM_NAME << " " << vec_str[ 2 ] << ") " << vec_str[ 3 ] << " " << vec_str[ 4 ] << ")";
 
-            tmp_str.str( "" );
-            tmp_str << "(move (player " << TEAM_NAME << " " << object_str << ") " << x_str << " " << y_str << ")";
-
-            udp_client[ TRNR_UNUM - 1 ]->UDP_send( tmp_str.str() );
-            }
-
-        /*--------------------------------------------------
-        Move ball command
-        --------------------------------------------------*/
-        else if( ( input.find( "mb" ) == 0 )
-              && ( trainer_enbld           ) )
-            {
-            input.erase( 0, 3 );
-
-            x_str = input.substr( 0, input.find_first_of( " " ) );
-            input.erase( 0, input.find_first_of( " " ) + 1 );
-            y_str = input.substr( 0, input.length() );
-
-            tmp_str.str( "" );
-            tmp_str << "(move (ball) " << x_str << " " << y_str << ")";
-
-            udp_client[ TRNR_UNUM - 1 ]->UDP_send( tmp_str.str() );
+                    udp_client[ TRNR_UNUM - 1 ]->UDP_send( tmp_str.str() );
+                    }
+                }
+            else
+                {
+                cout << "Invalid Format: mv [b/p] [uniform] x y" << endl;
+                }           
             }
 
         /*--------------------------------------------------
         Raw server command
         --------------------------------------------------*/
-        else if( input.find( "rw" ) == 0 )
+        else if( vec_str[ 0 ].find( "rw" ) == 0 )
             {
             input.erase( 0, 3 );
 
@@ -320,3 +409,44 @@ system( "PAUSE" );
 return( 0 );
 
 }	/* main() */
+
+
+/** Split String Into Tokens
+* @param None
+* @pre None
+* @post Outputs to standard out the Winsock API error message
+* @return Returns A Queue Of Strings
+*/
+
+static vector< string > split_str   /* split string into tokens     */
+    ( 
+    string              in_str      /* input string                 */
+    )
+{
+/*----------------------------------------------------------
+Local Variables
+----------------------------------------------------------*/
+int                     position;   /* position                     */
+vector< string >        ret_val;    /* return value                 */
+
+/*----------------------------------------------------------
+Initialization
+----------------------------------------------------------*/
+position = in_str.find( " " );
+
+/*----------------------------------------------------------
+Split the string into tokens
+----------------------------------------------------------*/
+while( position != -1 )
+    {
+    ret_val.push_back( in_str.substr( 0, position ) );
+    in_str.erase( 0, position + 1 );
+
+    position = in_str.find( " " );
+    }
+
+ret_val.push_back( in_str.substr( 0, in_str.length() ) );
+
+return( ret_val );
+
+}   /* split_str() */
